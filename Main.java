ba335b7569ee25bbec2c3cbca9497a26ed547f50
Vansh Nagpal <2002vansh@gmail.com>
2023-05-16 22:22:40 -0400

    javadoc

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-      System.out.print("Line 1");
-      System.out.print("\r\rLine 2");
-  }
-}
--- parser.java
+++ parser.java
@@ -1,3 +1,8 @@
+
+/**
+ * @author vnagpal
+ * Parser class converts a .txt file containing words to a .json file
+ */
 import java.io.FileReader;
 import java.io.File;
 import java.util.Scanner;
@@ -5,20 +10,26 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.io.FileWriter;
 
-public class parser {
+public class Parser {
 
   public static void main(String[] args) {
+    // main array list of all words read from .txt file
     ArrayList<String> allWords = new ArrayList<String>();
     try {
+      // infile reader
       File inFile = new File("dictionary.txt");
       FileReader reader = new FileReader(inFile);
       Scanner scanner = new Scanner(reader);
 
+      // reads each line, splits by space delimiter, adds it to the array list
       while (scanner.hasNextLine()) {
         String line = scanner.nextLine();
         String[] lineWords = line.split(" ");
+
+        // converts [] to ArrayList
         ArrayList<String> lineWordsList = new ArrayList<String>(Arrays.asList(lineWords));
 
+        // adds each line's array list to the main list
         allWords.addAll(lineWordsList);
       }
 
@@ -28,13 +39,19 @@ public class parser {
     }
 
     try {
+      // outfile writer
       File outFile = new File("dictionary.json");
       FileWriter writer = new FileWriter(outFile);
 
+      // opening bracket
       writer.write("[");
+      // writes each word, comma delimited enclosed in quotes
       for (int i = 0; i < allWords.size() - 1; i++)
         writer.write("\"" + allWords.get(i) + "\",");
-      writer.write("\"" +allWords.get(allWords.size() - 1) + "\"]");
+
+      // last word and closing bracket
+      writer.write("\"" + allWords.get(allWords.size() - 1) + "\"]");
+
       writer.close();
     } catch (Exception e) {
       e.printStackTrace();
