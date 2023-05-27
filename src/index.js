72e5490d05db41fc4313d702303d0beea1fdfbde
Vansh Nagpal <2002vansh@gmail.com>
2023-05-26 23:11:01 -0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,59 +1,48 @@
-//selects all input elements with the class specified by letter-input (which will be each individual box within each row)
-const letterInputs = document.querySelectorAll('.letter-input');
-
-//loops through each input element
-letterInputs.forEach(input => {
-  //listens for value being inputted into the box
-  input.addEventListener('input', function () {
-    //if the input length goes over 1, then it displays only the first characters
-    if (this.value.length > 1)
-      this.value = this.value[0];
-
-    // Remove non-alphabetic characters and converts letters to uppercase
-    /*regex: /[^a-zA-Z]/
-      []  : anything between here matches a single character [..][..] matches two characters 
-      /   : used to effectively bookmark the regex
-      ^   : specifies inversion (anything that doesn't match the following pattern)
-      a-z : specifies all lowercase letters
-      A-Z : specifies all uppercase letters
-      /g  : matches all occurences in the input string not the first one, redundant in this case
-    */
-    this.value = this.value.replace(/[^a-zA-Z]/g, '').toUpperCase();
-  });
-});
-
 function handleInput(event) {
   const input = event.target;//box in which value is being entered
   const value = input.value;//character being entered in the box
+  const correctInputForm = /[a-zA-Z]/;
+  const isLetter = correctInputForm.test(value);
+  if (isLetter)
+    input.value = value.toUpperCase();
 
-  if (value.length === 1) {
+  if (value.length === 1 && isLetter) {
     const currentDiv = input.parentNode;
     const rowInputs = Array.from(currentDiv.getElementsByClassName('letter-input'));
 
-    const nextBoxIndex = rowInputs.indexOf(currentInput) + 1;
+    const nextBoxIndex = rowInputs.indexOf(input) + 1;
 
     if (nextBoxIndex < rowInputs.length) {
       const nextBox = rowInputs[nextBoxIndex];
       nextBox.focus();
     }
   }
+  else {
+    input.value = '';
+  }
 }
 
 function handleKeyDown(event) {
   const key = event.key;
   const input = event.target;
-  const currentDiv = input.parentNode;
-  const rowInputs = Array.from(currentDiv.getElementsByClassName('letter-input'));
 
   switch (key) {
+    case 'Enter':
+      //user guess
+      registerGuess();
+      break;
+
     case 'Backspace':
-      const nextBoxIndex = rowInputs.indexOf(currentInput) - 1;
-      if (nextBoxIndex >= 0) {
-        const nextBox = rowInputs[nextBoxIndex];
-        nextBox.focus();
+      //condition being checked, current input is empty and there is a previous element
+      if (input.value === '' && input.previousElementSibling) {
+        const previousInput = input.previousElementSibling;
+        previousInput.value = '';
+        previousInput.focus();
       }
       break;
+
     default:
       break;
   }
 }
+
