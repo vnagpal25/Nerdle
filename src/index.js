3e7179e9f05f249fe353f2ee7e6ec0ce246cd593
Vansh Nagpal <2002vansh@gmail.com>
2023-06-05 12:27:36 -0400

    row switches are seamless

--- src/index.js
+++ src/index.js
@@ -2,35 +2,73 @@ import { fetchRandomWord as nerdleWord, getGuess, interpretGuess, populateWordHa
 let guess;
 let letterCounts;
 let gameWord;
+let activeRow = 1;// flag keeps track of current row
+const letterInputs = document.querySelectorAll('.letter-input');
+const nerdleRows = document.querySelectorAll('.input-row');
 
+//gets the nerdle word from WordnikAPI and populates the hashmap of letter counts also
 nerdleWord().then(word => {
   letterCounts = populateWordHash(word);
   gameWord = word;
 });
 
+//places cursor on the first input box
+focusCursor();
 
-const letterInputs = document.querySelectorAll('.letter-input');
+
+//disables rows 2-6 so user can only enter their guess in the first row(activeRow = 1)
+disableUneditableRows();
+
+//linking defined functions to user I/O
 letterInputs.forEach(input => {
-  input.oninput = handleInput; // Use 'oninput' instead of 'addEventListener'
-  input.onkeydown = handleKeyDown; // Use 'onkeydown' instead of 'addEventListener'
+  input.oninput = handleInput;
+  input.onkeydown = handleKeyDown;
 });
 
+function focusCursor() {
+  const currentRow = nerdleRows[activeRow - 1];
+  const firstInput = currentRow.querySelector('.letter-input');
 
+  //slight time delay to allow for the DOM to finish rendering
+  setTimeout(() => {
+    firstInput.focus();
+  }, 0);
+}
 
+//disables rows based on the activeRow variable
+function disableUneditableRows() {
+  //need to disable all other rows besides the currrent one
+  nerdleRows.forEach((row, index) => {
+    //gets all of the input elements in specific row
+    const rowInputs = row.querySelectorAll('.letter-input');
+
+    //this boolean will be T for all elements in the activeRow, and F for all elements in other rows
+    //this is because it matches the specifc row's index with the active row's index
+    const isEditable = (index === (activeRow - 1));
+
+    //disables all input elements in the specific row
+    rowInputs.forEach(input => { input.disabled = !isEditable });
+  });
+}
+
+//this function handles user input in the input elements
 function handleInput(event) {
   const input = event.target;//box in which value is being entered
   const value = input.value;//character being entered in the box
 
   const correctInputForm = /[a-zA-Z]/;//regex to match input
   const isLetter = correctInputForm.test(value);//checking if input is a letter
+
   //uppercases for uniformity
   if (isLetter)
     input.value = input.value.toUpperCase();
 
+  //automatically moves the cursor to the next element
   if (value.length === 1 && isLetter) {
     const currentDiv = input.parentNode;
     const rowInputs = Array.from(currentDiv.getElementsByClassName('letter-input'));
 
+    //index of next input element within the row
     const nextBoxIndex = rowInputs.indexOf(input) + 1;
 
     if (nextBoxIndex < rowInputs.length) {
@@ -49,9 +87,15 @@ function handleKeyDown(event) {
 
   switch (key) {
     case 'Enter':
-      //returns guess if it is valid (5 chars and english word) otherwise returns falsy empty string
+      //returns guess if it is valid (5 chars & english word) otherwise returns falsy empty string
       getGuess(event).then(guess => {
         if (guess) {
+          //win condition
+          //TODO
+          if(guess === gameWord){
+            //disable rows after the current row
+            //give a congratulations message to the user
+          }
           //changes color of the input boxes and lock it
           interpretGuess(letterCounts, guess, gameWord, event);
           //need to go to the next row now
@@ -64,6 +108,7 @@ function handleKeyDown(event) {
 
     case 'Backspace':
       //condition being checked, current input is empty and there is a previous element
+      //should only go to the previous square if the current square is empty
       if (input.value === '' && input.previousElementSibling) {
         const previousInput = input.previousElementSibling;
         previousInput.value = '';
@@ -87,11 +132,21 @@ function toNextRow(currRowID) {
 
   if (nextRowNum <= 6) {
     //get next div by its ID
-    const nextRowDiv = document.getElementById(nextRowID);
+    // const nextRowDiv = document.getElementById(nextRowID);
 
     //getting the array of inputs from the div 
-    const nextRowInputs = Array.from(nextRowDiv.getElementsByClassName('letter-input'));
+    // const nextRowInputs = Array.from(nextRowDiv.getElementsByClassName('letter-input'));
 
-    nextRowInputs[0].focus();
+    //puts the cursor in the first box of the next row
+    // nextRowInputs[0].focus();
+
+    //increments the activeRow variable and disables subsequent rows
+    activeRow++;
+    focusCursor();
+    disableUneditableRows();
+  }
+  else {
+    //the user has lost the game and hasn't gotten it within 6 tries
+    //display a 'you lost' message and type out what the word was
   }
 }
